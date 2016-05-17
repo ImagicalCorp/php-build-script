18a0792d667d2b4d2a00a1932e874775d4f6a669
Keith <keithkfng@users.noreply.github.com>
2016-05-17 20:04:04 +1000

    That should solve all the errors. Forgot "; then"

--- install.sh
+++ install.sh
@@ -96,9 +96,9 @@ w="install_log/log_wget"
 wp="install_log/log_wget_php"
 
 	echo $im_install_echo
-	if [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]
+	if [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]; then
 	curl -O https://github.com/ImagicalCorp/ImagicalMine/archive/master.zip >>./$w 2>>./$w
-	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]
+	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]; then
 	wget https://github.com/ImagicalCorp/ImagicalMine/archive/master.zip >>./$w 2>>./$w
 	else
 	curl -O https://github.com/ImagicalCorp/ImagicalMine/archive/master.zip >>./$w 2>>./$w
@@ -112,10 +112,10 @@ wp="install_log/log_wget_php"
 	cd .. >>../$l 2>>../$le
 	rm -rf ImagicalMine-master >>./$l 2>>./$le
 	rm -rf master.zip >>./$l 2>>./$le
-	if [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]
+	if [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]; then
 	curl -O --insecure https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/start.sh >>./$l 2>>./$le
 	curl -O --insecure https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/LICENSE.md >>./$l 2>>./$le
-	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]
+	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]; then
 	curl -O --insecure https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/start.sh >>./$l 2>>./$le
 	curl -O --insecure https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/LICENSE.md >>./$l 2>>./$le
 	else
@@ -125,15 +125,14 @@ wp="install_log/log_wget_php"
         chmod 777 start.sh >>./$l 2>>./$le
 	echo
 	echo $php_install_echo
-	if [ "$z" == "RPI2" ];then
+	if [ "$z" == "RPI2" ]; then
         wget --no-check-certificate https://raw.githubusercontent.com/keithkfng/my-files-and-icons/master/raspberry_php.tar.gz >>./$wp 2>>./$wp
         chmod 777 rasp* >>./$lp 2>>./$lpe
 	tar zxvf rasp* >>./$lp 2>>./$lpe
 	rm -r rasp* >>./$lp 2>>./$lpe
-	else
-	if [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]
+	elif [ "$z" == "PHP_7.0.2_x86_Linux.tar.gz" ]; then
 	curl -O --insecure https://bintray.com/artifact/download/pocketmine/PocketMine/$z >>./$wp 2>>./$wp
-	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]
+	elif [ "$z" == "PHP_7.0.2_x86-64_Linux.tar.gz" ]; then
 	curl -O --insecure https://bintray.com/artifact/download/pocketmine/PocketMine/$z >>./$wp 2>>./$wp
 	else
 	wget --no-check-certificate https://bintray.com/artifact/download/pocketmine/PocketMine/$z >>./$wp 2>>./$wp
