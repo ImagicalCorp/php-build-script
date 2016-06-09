0f30befb2975263b4eacc07f6450dc15f15cf750
Keith <keithkfng@users.noreply.github.com>
2016-06-10 06:27:44 +1000

    Create plugswork.sh

---
+++ plugswork.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# ImagicalMine & Plugswork Installation Script for Linux x86_64(master)
+#  _    _            _    _______ 
+# | |  | |    /\    | |  |__   __|
+# | |__| |   /  \   | |     | |   
+# |  __  |  / /\ \  | |     | |   
+# | |  | | / ____ \ | |____ | | _ 
+# |_|  |_|/_/    \_\|______||_|(_)
+# 
+# This file is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+# Before you start doing anything, read the license for more detail into what you are allowed to do and not do.
+while :
+do
+clear
+cat << "EOF"
+
+  _                       _           _ __  __ _             
+ (_)                     (_)         | |  \/  (_)            
+  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
+ | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
+ | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
+ |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
+                     __/ |                                   
+                    |___/   
+  
+EOF
+
+# shopt -s extglob
+echo "system> Welcome to ImagicalMine and Plugswork!"
+echo "system> This installer will automatically install ImagicalMine and Plugswork for your server!"
+echo "system> Ensure you are running Linux 64-bit, or the installer will not install properly."
+z="PHP_7.0.3_x86-64_Linux.tar.gz"
+l="install_log/log"
+le="install_log/log_errors"
+lp="install_log/log_php"
+lpe="install_log/log_php_errors"
+w="install_log/log_wget"
+wp="install_log/log_wget_php"
+
+	mkdir install_log
+	echo "system> Installing ImagicalMine..."
+	wget --no-check-certificate https://github.com/ImagicalCorp/ImagicalMine/archive/master.zip >>./$w 2>>./$w
+        chmod 777 master.zip >>./$l 2>>./$le
+	unzip -o master.zip >>./$l 2>>./$le
+	chmod 777 ImagicalMine-master >>./$l 2>>./$le
+	cd ImagicalMine-master >>./$l 2>>./$le
+	chmod 777 src >>../$l 2>>../$le
+	cp -rf src .. >>../$l 2>>../$le
+	cd .. >>../$l 2>>../$le
+	rm -rf ImagicalMine-master >>./$l 2>>./$le
+	rm -rf master.zip >>./$l 2>>./$le
+        wget --no-check-certificate https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/start.sh >>./$l 2>>./$le
+        wget --no-check-certificate https://raw.githubusercontent.com/ImagicalCorp/ImagicalMine/master/LICENSE.md >>./$l 2>>./$le
+        chmod 755 start.sh >>./$l 2>>./$le
+	echo
+	echo "system> Installing PHP binary..."
+	wget --no-check-certificate https://dl.bintray.com/pocketmine/PocketMine/$z >>./$wp 2>>./$wp
+	chmod 777 PHP* >>./$lp 2>>./$lpe
+	tar zxvf PHP* >>./$lp 2>>./$lpe
+	rm -r PHP* >>./$lp 2>>./$lpe
+	echo
+        echo "system> Installing Plugswork..."
+        
+        echo "system> ImagicalMine installation completed! Run ./start.sh (or ./st*) to start ImagicalMine."
+exit 0
+done
