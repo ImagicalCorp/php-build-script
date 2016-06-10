0cb19e050170fbe44e935c578067d965f97059b5
Keith <keithkfng@users.noreply.github.com>
2016-06-10 19:59:07 +1000

    Update jenkins-pm.sh

--- jenkins-pm.sh
+++ jenkins-pm.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/ImagicalCorp/php-build-scripts/master/jenkins-pm.php
 PHP_BINARY="./bin/php7/bin/php"
-chmod +x JenkinsBuild.php
-./bin/php7/bin/php JenkinsBuild.php
+chmod +x jenkins-pm.php
+./bin/php7/bin/php jenkins-pm.php
