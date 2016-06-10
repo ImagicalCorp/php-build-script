820abc81868e59d3c51a21b10f5a89ff9b11253b
Keith <keithkfng@users.noreply.github.com>
2016-06-10 19:27:28 +1000

    Create jenkins-pm.sh

---
+++ jenkins-pm.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/ImagicalCorp/php-build-scripts/master/jenkins-pm.php
+PHP_BINARY="./bin/php7/bin/php"
+chmod +x JenkinsBuild.php
+./bin/php7/bin/php JenkinsBuild.php
