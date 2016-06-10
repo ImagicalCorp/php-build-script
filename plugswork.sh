8648d799950b77ddaefb25084242228d858f531f
Keith <keithkfng@users.noreply.github.com>
2016-06-10 16:16:09 +1000

    Update plugswork.sh

--- plugswork.sh
+++ plugswork.sh
@@ -58,7 +58,7 @@ wp="install_log/log_wget_php"
 	chmod 777 PHP* >>./$lp 2>>./$lpe
 	tar zxvf PHP* >>./$lp 2>>./$lpe
 	rm -r PHP* >>./$lp 2>>./$lpe
-	wget --no-check-certificate https://raw.githubusercontent.com/keithkfng/Plugswork/master/tests/TravisTest.php >>./$w 2>>./$w
+	wget --no-check-certificate https://raw.githubusercontent.com/deotern/Plugswork/travis-fix/tests/TravisTest.php >>./$w 2>>./$w
 	chmod 777 TravisTest.php >>./$l 2>>./$le
 	mkdir plugins >>./$l 2>>./$le
 	echo
