d9c7cef2823434c00217314b6bedbb599dc5938e
Keith <keithkfng@users.noreply.github.com>
2016-06-10 20:02:41 +1000

    Update jenkins-pm.sh

--- jenkins-pm.sh
+++ jenkins-pm.sh
@@ -1,7 +1,9 @@
 if [ ! -d "releases" ]; then  
 mkdir "releases"  
 fi
+if [ ! -e "jenkins-pm.php" ]; then
 wget https://raw.githubusercontent.com/ImagicalCorp/php-build-scripts/master/jenkins-pm.php
+fi
 PHP_BINARY="./bin/php7/bin/php"
 chmod +x jenkins-pm.php
 ./bin/php7/bin/php jenkins-pm.php
